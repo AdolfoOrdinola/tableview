100086e34535e74fab2ffe99f75d8f06a8c61fbb
macbookUser <macbookuser@macbookpro-10.local>
2017-12-08 17:35:46 -0600

    Un proyecto en el que se manipula un tableview

--- tableview/TableViewController.swift
+++ tableview/TableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TableViewController.swift
 //  tableview
 //
 //  Created by macbookUser on 16/10/17.
@@ -10,30 +10,37 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
+    var multiplicador = 0
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+ func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
-        var multiplicador = 1
-        let resultado = multiplicador * indexPath.row
-        cell.textLabel?.text = "\(resultado)"
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        
+        
+        //let resultado = multiplicador * indexPath.row
+        
+        
+        cell.textLabel?.text = "hola"
         return cell
     }
-
+    
 }
 
